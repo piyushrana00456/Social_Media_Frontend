bd5cff8dc10d4ff437892d1dbe9fa88b523c3273
piyush.rana <piyush.rana@sakhaglobal.com>
2024-06-22 13:50:21 +0530

    Private routes fix

--- src/components/PrivateRoute/index.jsx
+++ src/components/PrivateRoute/index.jsx
@@ -2,27 +2,27 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "@/context/AuthContext";
 
-const PrivateRoute = (WrappedComponent) => {
-   
-    return (props) =>{
-        const {user} = useAuth();
-        const { router} = useRouter();
-        const [loading, setLoading] = useState(true);
+const PrivateRoute = ({children}) => {
+        const {user, loading} = useAuth();
+        const router  = useRouter();
+        const [checkingAuth, setCheckingAuth] = useState(true);
+        console.log("Inside PrivateRoute", { user, loading });
 
         useEffect(() => {
+         if(!loading) {
           if(!user) {
+            console.log("Redirecting to login");
             router.push('/login')
           } else {
-            setLoading(false);
-          } 
-        },[user])
+           setCheckingAuth(false);
+          }
+         } 
+        },[user, router, loading])
 
-        if(loading) {
+        if(checkingAuth || loading) {
             return (<div>Loading...</div>)
         }
-
-        return user ? <WrappedComponent {...props}/> : null;
-    }
+    return <>{children}</>;
 }
 
 export default PrivateRoute;
