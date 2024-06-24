1d9ca2140687005b6b96a0e2e3041fd548a4f5cf
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-06-24 18:54:48 +0530

    added navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import NavbarComponent from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24`} style={{border: '1px solid red'}}>
+    <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
       <NavbarComponent/>
         Hello world
       </main>
