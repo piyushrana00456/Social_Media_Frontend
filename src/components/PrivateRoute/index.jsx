ff41217a3ee5fd541ea22d71bb489ea3e712a9c7
piyush.rana <piyush.rana@sakhaglobal.com>
2024-06-24 23:34:26 +0530

    added token to cookies

--- src/components/PrivateRoute/index.jsx
+++ src/components/PrivateRoute/index.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "@/context/AuthContext";
-
+import Cookies from "js-cookie";
 const PrivateRoute = ({children}) => {
-        const {user, loading} = useAuth();
+        const {loading} = useAuth();
         const router  = useRouter();
+        const user = Cookies.get('userData')
         const [checkingAuth, setCheckingAuth] = useState(true);
         console.log("Inside PrivateRoute", { user, loading });
 
