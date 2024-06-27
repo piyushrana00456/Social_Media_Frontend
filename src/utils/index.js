79e6fa1074448539b0e946d2b331982750fc52b4
piyush.rana <piyush.rana@sakhaglobal.com>
2024-06-27 23:17:43 +0530

    added otp verification page and add username key for login payload

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,3 +18,11 @@ export const getCookies = (key) => {
 
     return null;
 }
+
+export const validateEmail = (email) =>{
+    return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
