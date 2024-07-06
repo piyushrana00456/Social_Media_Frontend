3bf239f2627a07f5bc7f5b6417cbae13a56b3a84
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-07-06 18:43:07 +0530

    socket changes, added socket to userLogout event

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
+import { useEffect, useRef } from "react";
+import { io } from "socket.io-client";
 import "@/styles/globals.css";
+import { getCookies } from "@/utils";
 import { AuthProvider } from "@/context/AuthContext";
 import { useRouter } from "next/router";
 import { RESTRICTED_NAVBAR_PATHS } from "@/utils/contants";
@@ -7,15 +10,40 @@ import NavbarComponent from "@/components/Navbar/Navbar";
 export default function App({ Component, pageProps }) {
   
   const router = useRouter();
-
+  const socket = useRef();
+  const user = getCookies('userData');
   const showNavbar = !RESTRICTED_NAVBAR_PATHS.includes(router.pathname);
+
+  useEffect(() => {
+    if(user?.token){
+      
+      if(!socket.current) socket.current = io(process.env.NEXT_PUBLIC_BASE_URL);
+      
+      if(socket.current){
+        socket.current.emit("userConnected", user.token);
+      
+        socket.current.on("onlineUsers", (usersList) => {
+          // list of online users;
+          console.log("onlineUsers", {usersList});
+        })
+      }
+    
+      return () => {
+        if(socket.current){
+          socket.current.emit("userDisconnected", user.token);
+          socket.current.off("onlineUsers");
+          // socket.current.disconnect();
+        }
+      }
+      }
+  },[user])
   
   return (
     <AuthProvider>
       {
-        showNavbar && <NavbarComponent />
+        showNavbar && <NavbarComponent socket={socket.current} user={user}/>
       }
-      <Component {...pageProps} />
+      <Component {...pageProps} socket={socket.current} user={user}/>
     </AuthProvider>
   );
 }
